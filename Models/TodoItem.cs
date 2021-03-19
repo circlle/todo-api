1684f19c01df35617654153ded6de3e43508c1f4
kxzhang <kxzhang@trip.com>
2021-03-19 19:59:01 +0800

    init

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+namespace todo_api.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+
+    public class TodoItemDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
